89a7de330c8c55e0aeee06ed7f80181c6408206e
Geremia <Geremia@users.noreply.github.com>
2026-02-08 16:23:48 -0700

    removed page size specifier in URL

--- HathiTrust.bash
+++ HathiTrust.bash
@@ -3,7 +3,7 @@
 for d in `seq ${1:?First Page} ${2:?Last Page}`
 do
   i=`printf "%03d" $d`
-  curl -Lo $i.png "https://babel.hathitrust.org/cgi/imgsrv/image?id=${3:?ID}&height=2092&seq=${d}" \
+  curl -Lo $i.png "https://babel.hathitrust.org/cgi/imgsrv/image?id=${3:?ID}&seq=${d}" \
     -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0' \
     -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' \
     -H 'Accept-Language: en-US,en;q=0.5' \
